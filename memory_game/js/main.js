71f8a90a1966f3eec7a85ac97e9609cdfb8352a2
Anna Lornitzo <Anna@floras-MacBook-Air.local>
2018-11-28 12:38:48 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -12,39 +12,61 @@ var cards = [
 {
 	rank: 'king',
 	suit: 'hearts',
-	cardImage: 'images/queen-of-hearts.png'
+	cardImage: 'images/king-of-hearts.png'
 },
 {
 	rank: 'king',
 	suit: 'diamonds',
 	cardImage: 'images/king-of-diamonds.png'
-},
+}
 
 ];
 var cardsInPlay = [];
+var score = 0;
+
 
 function checkForMatch() {
+	
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert('You found a match!');
+	score += 1;
+	document.getElementById('score').textContent = score;
 	} else {
 	alert('Sorry, try again.');
 	}
-}
+};
 
-function flipCard(cardId) {
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	} else {
 		return;
 	}
-	
-	
+};
 
-}
-console.log(cards)
-flipCard(0);
-flipCard(2);
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+var reset = function() {
+	document.getElementById('game-board').innerHTML = ""
+	cardsInPlay = [];
+	createBoard();
+};
+
+document.getElementById('reset').addEventListener('click', reset);
+
+createBoard();
